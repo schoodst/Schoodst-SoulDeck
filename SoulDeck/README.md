a2005eeb57ce607fd2c284ed30ac9a29ff691c16
Schoodst <96490994+schoodst@users.noreply.github.com>
2024-12-14 15:41:46 -0500

    Added the mod

---
+++ SoulDeck/README.md
@@ -0,0 +1,9 @@
+# Really stupid deck
+## Deck features
+- Stupid high spectral card appearances
+- starts with *7* Soul cards
+- almost 100 dollars off start (we love money)
+- 100+ joker slots
+- 100+ consumable slots
+- All you get is spades. NO other suits
+- Hand size to hold them ALL
---
+++ SoulDeck/SoulDeck.lua
@@ -0,0 +1,32 @@
+SMODS.Atlas{
+    key = "soulAtlas",
+    px = 71,
+    py = 95,
+    path = "SoulAtlas.png"
+}
+
+SMODS.Back{
+    name = "Soul Deck",
+    key = "souldeck",
+    pos = {x = 0, y = 0},
+    atlas = "soulAtlas",
+    config = {hand_size = 75, joker_slot = 100, consumable_slot = 100, dollars = 100, spectral_rate = 100, consumables = {"c_soul", "c_soul", "c_soul", "c_soul", "c_soul", "c_soul", "c_soul"}},
+    loc_txt = {
+        name = "Soul Deck",
+        text = {
+            "A Really Stupid Deck",
+        },
+    },
+    apply = function()
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                for k, v in ipairs(G.playing_cards) do
+                    if v.base.suit ~= "Spades" then
+                        v:change_suit("Spades")
+                    end
+                end
+            return true
+            end
+        }))
+    end
+}
